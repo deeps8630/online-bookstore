893879cca826ffe941add0c5ae18268cd4e1c697
SONIA <SONIA@LAPTOP-NIL5RPLL>
2020-04-29 11:46:39 +0530

    Springboot application with database connectivity

--- onlinebookstore/onlinebookstore/src/test/java/in/bushansirgur/onlinebookstore/OnlinebookstoreApplicationTests.java
+++ onlinebookstore/onlinebookstore/src/test/java/in/bushansirgur/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -1,13 +1,16 @@
 package in.bushansirgur.onlinebookstore;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
 @SpringBootTest
-class OnlinebookstoreApplicationTests {
+public class OnlinebookstoreApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
